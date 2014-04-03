f30d99b059323d42047958db99c71bfc7bb08ea6
Pat Newell <pnewell+git@renttherunway.com>
2014-04-03 16:05:08 -0400

    build

--- platforms/android/assets/www/js/controllers.js
+++ platforms/android/assets/www/js/controllers.js
@@ -1,23 +1,23 @@
 ;(function (angular, undefined) {
-	'use strict';
+	"use strict";
 
-	angular.module('sprtidApp.controllers', [])
-		.controller('HomeController', ["$scope", "$log", function($scope, $log) {
+	angular.module("sprtidApp.controllers", [])
+		.controller("HomeController", ["$scope", "$log", function($scope, $log) {
 			$log.info("HomeController", arguments);
 		}])
-		.controller('ScanController', ["$scope", "$log", function($scope, $log) {
+		.controller("ScanController", ["$scope", "$log", function($scope, $log) {
 			$log.info("ScanController", arguments);
 		}])
-		.controller('IdController', ["$scope", "$log", function($scope, $log) {
+		.controller("IdController", ["$scope", "$log", function($scope, $log) {
 			$log.info("IdController", arguments);
 		}])
-		.controller('CreateController', ["$scope", "$log", function($scope, $log) {
+		.controller("CreateController", ["$scope", "$log", function($scope, $log) {
 			$log.info("CreateController", arguments);
 		}])
-		.controller('ShopController', ["$scope", "$log", function($scope, $log) {
+		.controller("ShopController", ["$scope", "$log", function($scope, $log) {
 			$log.info("ShopController", arguments);
 		}])
-		.controller('SettingsController', ["$scope", "$log", function($scope, $log) {
+		.controller("SettingsController", ["$scope", "$log", function($scope, $log) {
 			$log.info("SettingsController", arguments);
 		}]);
 })(angular);
